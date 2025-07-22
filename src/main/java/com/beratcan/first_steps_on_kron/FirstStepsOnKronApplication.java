4605b773d7a8a865025642aab0cf66e64fa65e52
beratcan doğan <berat@beratcan-MacBook-Pro.local>
2025-07-22 14:31:05 +0300

    first commit

---
+++ src/main/java/com/beratcan/first_steps_on_kron/FirstStepsOnKronApplication.java
@@ -0,0 +1,13 @@
+package com.beratcan.first_steps_on_kron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsOnKronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstStepsOnKronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beratcan/first_steps_on_kron/FirstStepsOnKronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beratcan.first_steps_on_kron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepsOnKronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
